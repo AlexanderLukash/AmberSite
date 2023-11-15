89f6695b3d95be165422d55f1f0eaefca2a09392
AlexanderLukash <sashass20062727@gmail.com>
2023-11-15 11:09:29 +0200

    update

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 pip install virtualenv
 virtualenv venv
 source venv/bin/activate
+sudo apt-get install sqlite3
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate

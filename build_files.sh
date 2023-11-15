292f1afd0b237b22b6be54384e3e65c48c777f2a
AlexanderLukash <sashass20062727@gmail.com>
2023-11-15 10:59:35 +0200

    update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic
-python3.9 manage.py runserver
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+python manage.py runserver
